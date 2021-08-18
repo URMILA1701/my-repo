f386c670d8a2f0220aa60308529cd10123343892
URMILA1701 <urmila17jan2000@gmail.com>
2021-08-18 16:27:49 +0530

    initial

---
+++ HandsOnString/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HandsOnString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = Console.ReadLine();
+            String s1 = Console.ReadLine();
+            Console.WriteLine(s.Length);
+            Console.WriteLine(String.Compare(s1,s));
+            Console.WriteLine(s.ToUpper());
+            Console.WriteLine(s1.ToLower());
+            Console.WriteLine(s.Contains("ab"));
+            String[] words = { "abc", "def", "ghi", "jkl" };
+            Console.WriteLine(String.Concat(words));
+            char[] ch= s.ToCharArray();
+            foreach(char c in ch)
+                Console.WriteLine(c);
+            Console.WriteLine(s.EndsWith("i"));
+            Console.WriteLine(s.Equals("ghi"));
+            Console.WriteLine(s.IndexOf('h'));
+            Console.WriteLine(s.Insert(2,s1));
+            Console.WriteLine(s.LastIndexOf('h'));
+            Console.WriteLine(String.Join(",",words));
+            Console.WriteLine(s.Substring(0, 3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
